df4f1097d66c82fb33578d10824b41079b0a94e1
Dmitry Sviridov <sviridov.dvv@gmail.com>
2022-05-04 00:05:00 +0300

    Prepare project for frontend development

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,12 +1,55 @@
-const { ethers } = require('hardhat');
+const fs = require('fs');
+const path = require('path');
 
 async function main() {
+  if (network.name === 'hardhat') {
+    console.warn(
+      'You are trying to deploy a contract to the Hardhat Network, which' +
+        'gets automatically created and destroyed every time. Use the Hardhat' +
+        " option '--network localhost'"
+    );
+  }
+
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying with:', await deployer.getAddress());
+
+  console.log(
+    'Account balance:',
+    (await deployer.getBalance()).toString(),
+    '\n'
+  );
+
   const Transactions = await ethers.getContractFactory('Transactions');
   const transactions = await Transactions.deploy();
 
   await transactions.deployed();
 
   console.log('Transactions deployed to:', transactions.address);
+
+  saveFrontendFiles({
+    Transactions: transactions,
+  });
+}
+
+function saveFrontendFiles(contracts) {
+  const contractsDir = path.resolve(__dirname, '../../client/src/contracts');
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  for (key in contracts) {
+    fs.writeFileSync(
+      path.resolve(contractsDir, key + '-contract-address.json'),
+      JSON.stringify({ [key]: contracts[key].address }, undefined, 2)
+    );
+
+    const ContractArtifact = artifacts.readArtifactSync(key);
+    fs.writeFileSync(
+      path.resolve(contractsDir, key + '.json'),
+      JSON.stringify(ContractArtifact, null, 2)
+    );
+  }
 }
 
 main()
--- smart_contract/test/Transactions.test.js
+++ smart_contract/test/Transactions.test.js
@@ -2,16 +2,13 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('Transactions', function () {
-  let ownerAddr, otherAddr;
+  let owner, otherSigner;
   let transactions;
 
   beforeEach(async function () {
-    [ownerAddr, otherAddr] = await ethers.getSigners();
+    [owner, otherSigner] = await ethers.getSigners();
 
-    const Transactions = await ethers.getContractFactory(
-      'Transactions',
-      ownerAddr
-    );
+    const Transactions = await ethers.getContractFactory('Transactions', owner);
     transactions = await Transactions.deploy();
 
     await transactions.deployed();
@@ -27,10 +24,15 @@ describe('Transactions', function () {
     const ethAmount = 10000;
 
     await expect(() =>
-      transactions.addTransaction(otherAddr.address, 'My message!', 'keyword', {
-        value: ethAmount,
-      })
-    ).to.changeEtherBalances([ownerAddr, otherAddr], [-ethAmount, ethAmount]);
+      transactions.addTransaction(
+        otherSigner.getAddress(),
+        'My message!',
+        'keyword',
+        {
+          value: ethAmount,
+        }
+      )
+    ).to.changeEtherBalances([owner, otherSigner], [-ethAmount, ethAmount]);
   });
 
   it('Should add new transaction to history', async function () {
@@ -38,14 +40,21 @@ describe('Transactions', function () {
     const message = 'My message!';
     const keyword = 'keyword';
 
-    await transactions.addTransaction(otherAddr.address, message, keyword, {
-      value: ethAmount,
-    });
+    await transactions.addTransaction(
+      otherSigner.getAddress(),
+      message,
+      keyword,
+      {
+        value: ethAmount,
+      }
+    );
     let transactionHistory = await transactions.getTransactionHistory();
 
     expect(transactionHistory.length).to.equal(1);
-    expect(transactionHistory[0].sender).to.equal(ownerAddr.address);
-    expect(transactionHistory[0].reciever).to.equal(otherAddr.address);
+    expect(transactionHistory[0].sender).to.equal(await owner.getAddress());
+    expect(transactionHistory[0].reciever).to.equal(
+      await otherSigner.getAddress()
+    );
     expect(transactionHistory[0].amount).to.equal(ethAmount);
     expect(transactionHistory[0].message).to.equal(message);
     expect(transactionHistory[0].keyword).to.equal(keyword);
@@ -55,9 +64,14 @@ describe('Transactions', function () {
     const ethAmount = 10000;
 
     await expect(
-      transactions.addTransaction(otherAddr.address, 'My message!', 'keyword', {
-        value: ethAmount,
-      })
+      transactions.addTransaction(
+        otherSigner.getAddress(),
+        'My message!',
+        'keyword',
+        {
+          value: ethAmount,
+        }
+      )
     ).to.emit(transactions, 'TransactionAdded');
   });
 });
